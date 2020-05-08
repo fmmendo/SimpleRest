331b33038dd59d156d84cd1c7a086ee199484d83
Filipe Duarte <fmmendo@live.com>
2020-05-08 10:32:34 +0100

    remove commented out usings

--- SimpleRest/Http/Http.cs
+++ SimpleRest/Http/Http.cs
@@ -17,8 +17,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-//using RestSharp.Extensions;
-//using System.Net.Http;
 using Windows.Web.Http;
 using Mendo.UWP.Extensions;
 
